6776b758fce8a16b68c68d837a952a19c23d7c63
sanjay badlani <sanjaybadlani@sanjays-MacBook-Air.local>
2018-11-23 22:47:08 -0500

    "Commited the Database.sql file needed for creating the database"

---
+++ Database.sql
@@ -0,0 +1,116 @@
+CREATE DATABASE  IF NOT EXISTS `CAR_SENSOR` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `CAR_SENSOR`;
+-- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: CAR_SENSOR
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vehicle_alert`
+--
+
+DROP TABLE IF EXISTS `vehicle_alert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `vehicle_alert` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Vin` varchar(45) NOT NULL,
+  `alert_message` longtext NOT NULL,
+  `alert_priority` varchar(45) NOT NULL,
+  `current_timestamp_ts` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_alert`
+--
+
+LOCK TABLES `vehicle_alert` WRITE;
+/*!40000 ALTER TABLE `vehicle_alert` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_alert` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_detail`
+--
+
+DROP TABLE IF EXISTS `vehicle_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `vehicle_detail` (
+  `vin` varchar(45) NOT NULL,
+  `make` varchar(45) DEFAULT NULL,
+  `model` varchar(45) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `redline_rpm` int(11) DEFAULT NULL,
+  `max_fuel_volume` int(11) DEFAULT NULL,
+  `last_service_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`vin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_detail`
+--
+
+LOCK TABLES `vehicle_detail` WRITE;
+/*!40000 ALTER TABLE `vehicle_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_reading`
+--
+
+DROP TABLE IF EXISTS `vehicle_reading`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `vehicle_reading` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `vin` varchar(45) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `current_timestamp_ts` timestamp NULL DEFAULT NULL,
+  `Fuel_volume` float DEFAULT NULL,
+  `Speed` int(11) DEFAULT NULL,
+  `ENgine_hp` int(11) DEFAULT NULL,
+  `CHeck_engine_light_on` tinyint(1) DEFAULT NULL,
+  `ENgine_coolant_low` tinyint(1) DEFAULT NULL,
+  `Cruise_control_on` tinyint(1) DEFAULT NULL,
+  `ENgine_rpm` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_reading`
+--
+
+LOCK TABLES `vehicle_reading` WRITE;
+/*!40000 ALTER TABLE `vehicle_reading` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_reading` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-23 22:45:16
